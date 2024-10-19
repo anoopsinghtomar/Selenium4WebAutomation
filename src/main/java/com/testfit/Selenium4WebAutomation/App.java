98777019dfce2d6c04de546ab81404deaef59950
Anoop Singh <anoopsingh1387@gmail.com>
2024-10-20 02:46:59 +0530

    First Commit

---
+++ src/main/java/com/testfit/Selenium4WebAutomation/App.java
@@ -0,0 +1,10 @@
+package com.testfit.Selenium4WebAutomation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
