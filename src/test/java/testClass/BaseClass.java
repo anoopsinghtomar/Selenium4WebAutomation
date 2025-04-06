36f22ab81b0db9578f6440cbb26fc10900be99e6
Anoop Singh <anoopsingh1387@gmail.com>
2025-04-06 15:44:59 +0530

    cross browser & parallel testing

--- src/test/java/testClass/BaseClass.java
+++ src/test/java/testClass/BaseClass.java
@@ -6,10 +6,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import com.github.javafaker.Faker;
 
@@ -22,8 +25,16 @@ public class BaseClass {
 	public String password = faker.internet().password(5, 8, true);
 	
 	@BeforeTest
-	public void setup() {
-		driver = new ChromeDriver();
+	@Parameters({"os","browser"})
+	public void setup(String os, String browser) {
+		
+		switch(browser.toLowerCase()) {
+		case "chrome": driver = new ChromeDriver(); break;
+		case "edge": driver = new EdgeDriver(); break;
+		case "firefox": driver = new FirefoxDriver(); break;
+		default: System.out.println("invalid browser"); return;
+		}
+		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
